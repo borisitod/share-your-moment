286ec674dbfa9a13772d51e38124c26614cefe50
junchi chen <junchichen@junchis-MacBook-Pro.local>
2017-09-17 17:31:42 +1000

    clean up the app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,4 @@
-var deferredPrompt
+var deferredPrompt;
 
 if (!window.Promise) {
     window.Promise = Promise;
@@ -7,12 +7,12 @@ if (!window.Promise) {
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
-        .then(function() {
-          console.log('ServiceWorker is registered')
+        .then(function () {
+            console.log('Service worker registered!');
         })
         .catch(function(err) {
             console.log(err);
-        })
+        });
 }
 
 window.addEventListener('beforeinstallprompt', function(event) {
@@ -20,71 +20,4 @@ window.addEventListener('beforeinstallprompt', function(event) {
     event.preventDefault();
     deferredPrompt = event;
     return false;
-
-})
-
-var promise = new Promise(function(resolve, reject) {
-    setTimeout(function(){
-        //resolve('This is executed once the timer is done')
-        reject({code: 500, message: "Error !!!!!!!!!!!!!"})
-    }, 3000)
 });
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET','https://httpbin.org/ip');
-xhr.responseType = 'json';
-
-xhr.onload = function(){
-  console.log(xhr.response);
-};
-
-xhr.onerror = function() {
-    console.log('Error');
-}
-
-xhr.send();
-
-
-fetch('https://httpbin.org/ip')
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-    })
-
-fetch('https://httpbin.org/post', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    },
-    mode: 'cors',
-    body: JSON.stringify({message: 'Post Request'})
-})
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-    })
-
-// promise.then(function(text){
-//     return text;
-// }, function(err){
-//     console.log(err);
-// }).then(function(newText){
-//     console.log(newText)
-// });
-
-promise.then(function(text){
-    return text;
-}).then(function(newText){
-    console.log(newText)
-}).catch(function (err){
-    console.log(err)
-});
-
-console.log('This is executed right after setTimeout()')
