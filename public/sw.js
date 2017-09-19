3d56415299c5488bae12a8af43dddacc3c6e6d16
Boris <boris.chen@kdis.com>
2017-09-19 14:31:18 +1000

    cleaning/trimming cache

--- public/sw.js
+++ public/sw.js
@@ -17,6 +17,19 @@ var STATIC_FILES = [
     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
+function trimCache(cacheName, maxItems){
+    caches.open(cacheName)
+        .then(function (cache) {
+           return cache.keys()
+               .then(function (keys) {
+                   if (keys.length > maxItems) {
+                       cache.delete(keys[0])
+                           .then(trimCache(cacheName, maxItems))
+                   }
+               });
+        })
+}
+
 
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Innstalling the Service Worker', event);
@@ -85,6 +98,7 @@ self.addEventListener('fetch', function(event) {
                             .then(function (res) {
                                 return caches.open(CACHE_DYNAMIC_NAME)
                                     .then(function (cache) {
+                                        trimCache(CACHE_DYNAMIC_NAME, 3)
                                         cache.put(event.request.url, res.clone());
                                         return res;
                                     })
