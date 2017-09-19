c4f76b07d3a13b0892d7a6d9907f4cec17f96605
Boris <boris.chen@kdis.com>
2017-09-19 14:38:53 +1000

    comment some funtions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_NAME = 'static-v13' ;
+var CACHE_STATIC_NAME = 'static-v15' ;
 var CACHE_DYNAMIC_NAME = 'dynamic-v8' ;
 var STATIC_FILES = [
     '/',
@@ -17,18 +17,18 @@ var STATIC_FILES = [
     'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
-function trimCache(cacheName, maxItems){
-    caches.open(cacheName)
-        .then(function (cache) {
-           return cache.keys()
-               .then(function (keys) {
-                   if (keys.length > maxItems) {
-                       cache.delete(keys[0])
-                           .then(trimCache(cacheName, maxItems))
-                   }
-               });
-        })
-}
+// function trimCache(cacheName, maxItems){
+//     caches.open(cacheName)
+//         .then(function (cache) {
+//            return cache.keys()
+//                .then(function (keys) {
+//                    if (keys.length > maxItems) {
+//                        cache.delete(keys[0])
+//                            .then(trimCache(cacheName, maxItems))
+//                    }
+//                });
+//         })
+// }
 
 
 self.addEventListener('install', function(event) {
@@ -98,7 +98,7 @@ self.addEventListener('fetch', function(event) {
                             .then(function (res) {
                                 return caches.open(CACHE_DYNAMIC_NAME)
                                     .then(function (cache) {
-                                        trimCache(CACHE_DYNAMIC_NAME, 3)
+                                        //trimCache(CACHE_DYNAMIC_NAME, 3)
                                         cache.put(event.request.url, res.clone());
                                         return res;
                                     })
