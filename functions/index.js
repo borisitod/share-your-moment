dd67ba0c90497ba81792156f946c6fccd0049b3f
Boris <boris.chen@kdis.com>
2017-09-25 17:19:52 +1000

    adding firebase staff

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+var functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.storePostData = functions.https.onRequest(function(request, response)  {
+ response.send("Hello from Firebase!");
+});
