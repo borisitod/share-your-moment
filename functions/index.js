7f3a1e87e7b124dcd9823867b7365e02fed3a00c
junchi chen <junchichen@junchis-MacBook-Pro.local>
2017-10-01 18:54:06 +1100

    sending push messages from the server

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
-
+var webpush = require('web-push');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -21,6 +21,25 @@ exports.storePostData = functions.https.onRequest(function(request, response)  {
          image: request.body.image
      })
          .then(function () {
+             webpush.setVapidDetails('mailto:business@pcwgram.com',
+                 'BHYJESGUh8mHInmBIKXCJOsqk1TskX3Q-QnJX4aavpm-3rwpusb5thr6bMnVYz1VgF8VHFAN7NGBdO6N5hrJlF8',
+                 'wWSpcyQYkVAnLZv4g3W8FP0WVPsZVc_fdmSxLpi-URI');
+             return admin.database().ref('subscription').once('value');
+         })
+         .then(function (subscriptions) {
+             subscriptions.forEach(function (sub) {
+                var pushConfig = {
+                    endpoint: sub.val().endpoint,
+                    keys: {
+                        auth: sub.val().keys.auth,
+                        p256dh: sub.val().keys.p256dh
+                    }
+                };
+                webpush.sendNotification(pushConfig, JSON.stringify({title: 'New Post', content: 'New Post Added!'}))
+                    .catch(function (err) {
+                        console.log(err);
+                    })
+             });
              response.status(201).json({message: 'Data stored', id: request.body.id});
          })
          .catch(function (err) {
