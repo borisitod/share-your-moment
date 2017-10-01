74ff38bdb7f83e7ec97bf841717f89e26f3036ec
junchi chen <junchichen@junchis-MacBook-Pro.local>
2017-10-01 19:39:03 +1100

    sending new post message from the server

--- functions/index.js
+++ functions/index.js
@@ -12,38 +12,38 @@ admin.initializeApp({
     databaseURL: 'https://pwagram-49076.firebaseio.com/'
 });
 
-exports.storePostData = functions.https.onRequest(function(request, response)  {
- cors(request, response, function () {
-     admin.database().ref('posts').push({
-         id: request.body.id,
-         title: request.body.title,
-         location: request.body.location,
-         image: request.body.image
-     })
-         .then(function () {
-             webpush.setVapidDetails('mailto:business@pcwgram.com',
-                 'BHYJESGUh8mHInmBIKXCJOsqk1TskX3Q-QnJX4aavpm-3rwpusb5thr6bMnVYz1VgF8VHFAN7NGBdO6N5hrJlF8',
-                 'wWSpcyQYkVAnLZv4g3W8FP0WVPsZVc_fdmSxLpi-URI');
-             return admin.database().ref('subscription').once('value');
-         })
-         .then(function (subscriptions) {
-             subscriptions.forEach(function (sub) {
-                var pushConfig = {
-                    endpoint: sub.val().endpoint,
-                    keys: {
-                        auth: sub.val().keys.auth,
-                        p256dh: sub.val().keys.p256dh
-                    }
-                };
-                webpush.sendNotification(pushConfig, JSON.stringify({title: 'New Post', content: 'New Post Added!'}))
-                    .catch(function (err) {
-                        console.log(err);
-                    })
-             });
-             response.status(201).json({message: 'Data stored', id: request.body.id});
-         })
-         .catch(function (err) {
-             response.status(500).json({error: err});
-         })
- })
+exports.storePostData = functions.https.onRequest(function (request, response) {
+    cors(request, response, function () {
+        admin.database().ref('posts').push({
+            id: request.body.id,
+            title: request.body.title,
+            location: request.body.location,
+            image: request.body.image
+        })
+            .then(function () {
+                webpush.setVapidDetails('mailto:business@academind.com', 'BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE', 'AyVHwGh16Kfxrh5AU69E81nVWIKcUwR6a9f1X4zXT_s');
+                return admin.database().ref('subscriptions').once('value');
+            })
+            .then(function (subscriptions) {
+                subscriptions.forEach(function (sub) {
+                    var pushConfig = {
+                        endpoint: sub.val().endpoint,
+                        keys: {
+                            auth: sub.val().keys.auth,
+                            p256dh: sub.val().keys.p256dh
+                        }
+                    };
+
+                    webpush.sendNotification(pushConfig, JSON.stringify({title: 'New Post', content: 'New Post added!'}))
+                        .catch(function(err) {
+                            console.log(err);
+                        })
+                });
+                response.status(201).json({message: 'Data stored', id: request.body.id});
+            })
+            .catch(function (err) {
+                response.status(500).json({error: err});
+            });
+    });
 });
+
